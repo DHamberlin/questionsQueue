b549ac9aca8349ec940ec277ca6f8c5c68d85b36
David H <davidhamberlin@gmail.com>
2017-05-04 11:35:47 -0400

    refactor complete

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,16 +1,16 @@
-const router = require('express').Router();
+const routs = require('express').Router();
 const controllers = require('./controllers');
 const passport = require('passport');
 
-router.get('/auth/github', passport.authenticate('github'));
-router.get('/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/auth/github' }),
-    (req, res) => res.redirect('/'));
+routs.get('/auth/github', passport.authenticate('github'));
+routs.get('/auth/github/callback',
+            passport.authenticate('github', { failureRedirect: '/auth/github' }),
+            (req, res) => res.redirect('/'));
 
-router.route('/api/questions')
+routs.route('/api/questions')
   .get(controllers.getQuestions)
   .post(controllers.postQuestion)
   .put(controllers.updateQuestion)
   .delete(controllers.deleteQuestion);
 
-module.exports = router;
+module.exports = routs;
