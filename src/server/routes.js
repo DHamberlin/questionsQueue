38476d43984affdc78b43907e5c7d284bb93066f
David H <davidhamberlin@gmail.com>
2017-05-04 00:08:09 -0400

    server reorg cleanup

--- src/server/routes.js
+++ src/server/routes.js
@@ -3,7 +3,6 @@ const controllers = require('./controllers');
 const passport = require('passport');
 
 router.get('/auth/github', passport.authenticate('github'));
-
 router.get('/auth/github/callback',
   passport.authenticate('github', { failureRedirect: '/auth/github' }),
     (req, res) => res.redirect('/'));
--- src/server/server.js
+++ src/server/server.js
@@ -1,38 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const passport = require('passport');
 const path = require('path');
+const passport = require('passport');
 const morgan = require('morgan');
 
 const routes = require('./routes');
 const auth = require('./auth');
 const config = require('../../config');
 
-const passport = require('passport');
-const GithubStrategy = require('passport-github').Strategy;
-const config = require('./config');
-const session = require('express-session');
-
-passport.use(new GithubStrategy({
-  clientID: config.githubID,
-  clientSecret: config.githubSecret,
-  callbackURL: '/auth/github/callback' },
-  (accessToken, refreshToken, profile, done) => done(null, profile)));
-
-passport.serializeUser((user, done) => done(null, user));
-passport.deserializeUser((user, done) => done(null, user));
-// ^^^^^^ this may be the place to check user in db ^^^^^
-
-function ensureAuth(req, res, next) {
-  console.log('Authenticated: ', req.isAuthenticated());
-  console.log('Headers: ', req.headers);
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/auth/github');
-  return 'appease airbnb';
-}
-
-const port = process.env.PORT || 8080;
 const app = express();
 
 // configure passport to run on all requests
@@ -48,6 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(routes);
 
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
 });
